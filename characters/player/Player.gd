fb9b39caabd8bce97cd5ac11e5e2f54100297e63
abraham.lai <105269475+abewtsa@users.noreply.github.com>
2025-12-30 18:09:26 +1100

    rearrange assets folder

--- characters/player/Player.gd
+++ characters/player/Player.gd
@@ -18,7 +18,6 @@ var can_move = true
 
 func _ready() -> void:
 	ManagerGame.global_player_ref = self
-	
 	hurtbox.hp = hp_max
 
 
@@ -27,7 +26,7 @@ func _physics_process(delta: float) -> void:
 		attack()
 	if Input.is_action_just_pressed("roll"):
 		state_machine.change_state_by_name('Roll')
-	
+
 	$Aim.look_at(global_position + direction)
 
 
@@ -35,7 +34,7 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_released('mouse_up'):
 		if $Camera2D.zoom < Vector2(2, 2):
 			$Camera2D.zoom += Vector2(0.1, 0.1)
-	
+
 	if event.is_action_released('mouse_down'):
 		if $Camera2D.zoom > Vector2(.7, .7):
 			$Camera2D.zoom -= Vector2(0.1, 0.1)
@@ -48,17 +47,17 @@ func attack():
 func death():
 	hurtbox.disable()
 	can_move = false
-	
+
 	anim_tree.active = false
 	$AnimationPlayer.active = true
 	$AnimationPlayer.play("death")
-	
+
 	get_tree().paused = true
 
 
 func _on_hurtbox_hit() -> void:
 	$HitFX.play("hit")
-	
+
 	ManagerGame.global_ui_ref.refresh_hearts_display()
 
 
