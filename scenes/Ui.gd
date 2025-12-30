1c4f2073f9382f0c92cf5c77339649aa20bf7861
abraham.lai <105269475+abewtsa@users.noreply.github.com>
2025-12-30 17:35:21 +1100

    rearrange pause menu folder

--- scenes/Ui.gd
+++ scenes/Ui.gd
@@ -5,9 +5,9 @@ class_name UI
 
 func _ready() -> void:
 	ManagerGame.global_ui_ref = self
-	
+
 	await get_tree().physics_frame
-	
+
 	refresh_hearts_display()
 
 
@@ -18,17 +18,17 @@ func _physics_process(delta: float) -> void:
 func refresh_hearts_display():
 	for i in $HeartsBox.get_children():
 		i.queue_free()
-	
+
 	for i in ManagerGame.global_player_ref.hurtbox.hp:
 		var e = preload('res://assets/ui/Heart.tscn').instantiate()
 		e.texture = load('res://resources/icons/heart.tres')
-		
+
 		$HeartsBox.add_child(e)
-	
+
 	#for i in ManagerGame.global_player_ref.hurtbox.hp:
 		#$HeartsBox.get_child(i).texture = load('res://resources/icons/heart.tres')
 
 
 func _on_pause_pressed() -> void:
-	var i = preload('res://components/ui_popups/PauseMenu.tscn').instantiate()
+	var i = preload('res://assets/ui/pause_menuUI/PauseMenu.tscn').instantiate()
 	ManagerGame.pop_to_ui(i)
